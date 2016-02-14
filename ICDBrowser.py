4822af8f4c896cd04c9414c4bb298ef62893aae5
nicolas <nulysse4@gmail.com>
2016-02-14 15:59:00 -0500

    Configured treeview

--- ICDBrowser.py
+++ ICDBrowser.py
@@ -1,5 +1,6 @@
 
 from Tkinter import *
+import ttk
 
 __pad_x__ = 5
 __pad_y__ = 5
@@ -18,10 +19,18 @@ class Browser(Frame):
 
     def create_message_list(self):
         fm2 = Frame(self.master)
-        list = Listbox(fm2)
-        list.pack(expand=YES, fill=BOTH, padx=__pad_x__, pady=__pad_y__)
-        for item in range(70):
-            list.insert(END, {'test': 'x', 'toto': item})
+        t=ttk.Treeview(fm2)
+        t["columns"]=("first","second")
+        t.column("first")
+        t.column("second")
+        t.heading("first",text="first column")
+        t.heading("second",text="second column")
+        t.insert("",'end',text="directory 1",values=("file 1 A","file 1 B"))
+        t.insert("",'end',"dir2",text="directory 2",values=("file 1 A","file 1 B"))
+        t.tag_configure("ttk")
+        t["displaycolumns"]=("second","first")
+        t["show"]=('headings')
+        t.pack(fill=BOTH,expand=YES)
         fm2.pack(side=BOTTOM, expand=YES, fill=BOTH)
 
     def create_equipment_labels(self, frame):
